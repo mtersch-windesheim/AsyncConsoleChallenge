262dfae94770345da985d7157e225d855475cb63
Martijn ter Schegget <54362214+mtersch-windesheim@users.noreply.github.com>
2020-03-13 14:48:10 +0100

    Methodes toegevoegd.

--- AsyncConsoleChallenge/Program.cs
+++ AsyncConsoleChallenge/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace AsyncConsoleChallenge
 {
@@ -7,6 +9,19 @@ namespace AsyncConsoleChallenge
         static void Main(string[] args)
         {
             Console.WriteLine("We gaan een ontbijtje maken!");
+        }
+        static string SchenkKoffieIn() {
+            Thread.Sleep(500);
+            return "Koffie is klaar";
+        }
+        static async Task<string> KookEitjesAsync() {
+            await Task.Delay(3000);
+            return "Eitjes zijn klaar";
+        }
+        static async Task<string> RoosterBroodAsync()
+        {
+            await Task.Delay(3000);
+            return "Brood is klaar";
         }
     }
 }
